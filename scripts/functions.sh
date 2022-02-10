2ca7babefa25318642fba0371a0912eeb809c972
Christoph Deppisch <cdeppisch@redhat.com>
2022-02-10 16:34:06 +0100

    Introduce non-interactive toolchain setup
    Disable interactive mode on toolchain to avoid user confirmation prompts during the test run

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -18,7 +18,7 @@ create_standard_users() {
 
   pushd . > /dev/null
   cd $location && cd ../../toolchain-e2e || return
-  go run setup/main.go --users $num --batch $batch_num --default $workload --custom 0 --username $prefix
+  go run setup/main.go --interactive=false --users $num --batch $batch_num --default $workload --custom 0 --username $prefix
   popd > /dev/null || return
 }
 
@@ -43,7 +43,7 @@ create_custom_users() {
 
   pushd . > /dev/null
   cd $location && cd ../../toolchain-e2e || return
-  go run setup/main.go --template $full_template --users $num --batch $batch_num --default 0 --custom $num --username $prefix
+  go run setup/main.go --interactive=false --template $full_template --users $num --batch $batch_num --default 0 --custom $num --username $prefix
   popd > /dev/null || return
 }
 
@@ -82,7 +82,7 @@ create_users_with_custom_build() {
   do
     sed -e "s/build-property =.*$/build-property = $i/" ../camel-k-perf/templates/camel-build-template.yaml > /tmp/camel-build-$i.yaml
 
-    go run setup/main.go --template /tmp/camel-build-$i.yaml --users 1 --batch 1 --default 0 --custom 1 --username $prefix-$i
+    go run setup/main.go --interactive=false --template /tmp/camel-build-$i.yaml --users 1 --batch 1 --default 0 --custom 1 --username $prefix-$i
   done
 
   popd > /dev/null || return
