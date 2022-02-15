9747ce119e008cab771510b6740e0534cd24c434
Christoph Deppisch <cdeppisch@redhat.com>
2022-02-15 19:43:38 +0100

    More full Camel users

--- scripts/02-suddenly-love-for-camel.sh
+++ scripts/02-suddenly-love-for-camel.sh
@@ -8,5 +8,5 @@ location=$(dirname $0)
 source $location/functions.sh
 
 create_standard_users 1000 0 camel-02-empty
-create_custom_users camel-no-resources.yaml 990 camel-02-nores
-create_custom_users camel-full.yaml 10 camel-02-full
+create_custom_users camel-no-resources.yaml 900 camel-02-nores
+create_custom_users camel-full.yaml 100 camel-02-full
