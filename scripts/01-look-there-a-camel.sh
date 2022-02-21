843285a7d6f8fa3aa807b36ca40cc5944b482ae0
Christoph Deppisch <cdeppisch@redhat.com>
2022-02-21 12:59:10 +0100

    Adjust scenario 01-look-there-a-camel
    - Use 2000 users on the cluster
    - 1000 of the active users
    - 800 active non-Camel users
    - 200 active Camel users
    - 170 no resources
    - 20 full resources
    - 9 custom build
    - 1 integration platform

--- scripts/01-look-there-a-camel.sh
+++ scripts/01-look-there-a-camel.sh
@@ -7,8 +7,8 @@ echo "Running scenario 01: Look there's a Camel"
 location=$(dirname $0)
 source $location/functions.sh
 
-create_standard_users 1900 900 camel-01-normal
+create_standard_users 1800 800 camel-01-normal
 create_users_with_custom_build 9 camel-01-builds
+create_custom_users camel-no-resources.yaml 170 camel-01-nores
+create_custom_users camel-full.yaml 20 camel-01-full
 create_custom_users camel-platform.yaml 1 camel-01-platform
-create_custom_users camel-no-resources.yaml 80 camel-01-nores
-create_custom_users camel-full.yaml 10 camel-01-full
