63d0dbf2dc7cdec6a22e2b56ec313ead61cd40a7
Christoph Deppisch <cdeppisch@redhat.com>
2022-03-01 10:24:55 +0100

    Adjust number of Camel users in test scenarios
    - Do not create more than 600 active Camel K users on the cluster
    - The test cluster is hitting max pod per nodes limit when creating 600+ users with Camel K integrations as user workload. This is because the idler coming with the codeready toolchain member operator has to be disabled due to lack of support for scale sub-resources. With the idler being disabled no pods are killed during the performance test run.

--- scripts/03-the-summit-live-workshop.sh
+++ scripts/03-the-summit-live-workshop.sh
@@ -8,4 +8,4 @@ location=$(dirname $0)
 source $location/functions.sh
 
 create_standard_users 2000 0 camel-03-nores
-inject_peak_workload camel-no-resources.yaml 1000 camel-03-nores
+inject_peak_workload camel-no-resources.yaml 600 camel-03-nores
--- scripts/04-good-cameleers-share-builds.sh
+++ scripts/04-good-cameleers-share-builds.sh
@@ -7,7 +7,7 @@ echo "Running scenario 04: Good cameleers share builds"
 location=$(dirname $0)
 source $location/functions.sh
 
-create_standard_users 1000 0 camel-04-empty
-create_standard_users 950 0 camel-04-nores
+create_standard_users 1400 0 camel-04-empty
+create_standard_users 550 0 camel-04-nores
 create_users_with_custom_build 50 camel-04-builds
-inject_peak_workload camel-no-resources.yaml 950 camel-04-nores
+inject_peak_workload camel-no-resources.yaml 550 camel-04-nores
