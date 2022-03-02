17919e600860fc0a5286742b38c1f9aef972ac09
Christoph Deppisch <cdeppisch@redhat.com>
2022-03-02 19:29:43 +0100

    Need to wait for users to be fully provisioned before injecting workload

--- scripts/04-good-cameleers-share-builds.sh
+++ scripts/04-good-cameleers-share-builds.sh
@@ -9,5 +9,8 @@ source $location/functions.sh
 
 create_standard_users 1400 0 camel-04-empty
 create_standard_users 550 0 camel-04-nores
+
+sleep 120s
+
 inject_peak_workload camel-no-resources.yaml 550 camel-04-nores
 create_users_with_custom_build 50 camel-04-builds
--- scripts/05-the-bad-workshop.sh
+++ scripts/05-the-bad-workshop.sh
@@ -9,4 +9,7 @@ source $location/functions.sh
 
 create_standard_users 1900 0 camel-05-empty
 create_standard_users 100 0 camel-05-platform
+
+sleep 120s
+
 inject_peak_workload camel-platform.yaml 100 camel-05-platform
