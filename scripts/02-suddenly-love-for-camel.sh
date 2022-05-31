81a1284a87ff7d718bc0cb56e4e8aec575c42d51
Christoph Deppisch <cdeppisch@redhat.com>
2022-05-31 12:48:53 +0200

    Use the full integration template

--- scripts/02-suddenly-love-for-camel.sh
+++ scripts/02-suddenly-love-for-camel.sh
@@ -7,5 +7,5 @@ echo "Running scenario 02: Suddenly love for Camel"
 location=$(dirname $0)
 source $location/functions.sh
 
-create_standard_users 1400 1400 camel-02-normal
-create_custom_users camel-no-resources.yaml 600 camel-02-nores
+create_standard_users 1400 0 camel-02-normal
+create_custom_users camel-full.yaml 600 camel-02-full
